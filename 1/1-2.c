29162a1c49bc84592d2201bfcfc7b5c10d70c9ea
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-09-15 15:27:25 -0300

    1-2

---
+++ 1/1-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main() {
+  
+  printf("\a");
+  printf("\b");
+  printf("\e");
+  printf("\f");
+  printf("\n");
+  printf("\r");
+  printf("\t");
+  printf("\v");
+  printf("\\");
+  printf("\'");
+  printf("\"");
+  printf("\?");
+      
+}
