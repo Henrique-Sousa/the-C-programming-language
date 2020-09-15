431486d9f5da0b2636aea1abe2ff13d76d3644ce
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-09-15 16:45:16 -0300

    more elegant version

--- chapter-1/1-9.c
+++ chapter-1/1-9.c
@@ -4,15 +4,19 @@ main() {
   int c, last_was_blank;
 
   last_was_blank = 0;
-  while((c = getchar()) != EOF)
-    if(last_was_blank == 0) {
+  while((c = getchar()) != EOF) {
+
+    if ((last_was_blank == 1) && (c == ' ')) {
+      ;
+    } else {
       putchar(c);
-      if (c == ' ')
-        last_was_blank = 1;
-      else 
-        last_was_blank = 0;
-    } else if (c != ' ') {
-      putchar(c);
+    }
+
+    if (c == ' ') {
+      last_was_blank = 1;
+    } else {
       last_was_blank = 0;
-    } 
+    }
+
+  }
 }
