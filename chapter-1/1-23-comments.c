922e03f6f543c77befe86f697e82b7cde4a92eec
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-09-17 17:30:26 -0300

    replace nested ifs by if elses. fix: previous versions were not handling divison / operator correctly.

--- chapter-1/1-23-comments.c
+++ chapter-1/1-23-comments.c
@@ -2,62 +2,59 @@
 
 int main() {
 
-  int c, singlequoted, doublequoted, escaped, slashed, commented, asterisk;
-  
-  escaped = doublequoted = singlequoted = slashed = commented = asterisk = 0;
+    int c, singlequoted, doublequoted, escaped, slashed, commented, asterisk;
+    
+    escaped = doublequoted = singlequoted = slashed = commented = asterisk = 0;
 
-  while ((c = getchar()) != EOF) {
-    if (commented) {
-      if (asterisk) {
-        if (c == '/') {
-          asterisk = 0;
-          commented = 0;
-        }
-      } else {
-        if (c == '*') {
-          asterisk = 1;
-        }
-      }
-    } else {
-      if (doublequoted) {
-        if (escaped) {
-          escaped = 0;
-        } else {
-          if (c == '\"') {
-            doublequoted = 0;
-          }
-          if (c == '\\') {
-            escaped = 1;
-          }    
-        } 
-      } else {
-        if (singlequoted) {
-          if (c == '\'') {
-            singlequoted = 0;
-          }
-        } else {
-          if (slashed) {
-            if (c == '*') {
-              slashed = 0;
-              commented = 1;
-              continue;
+    while ((c = getchar()) != EOF) {
+        if (commented) {
+            if (asterisk) {
+                if (c == '/') {
+                    asterisk = 0;
+                    commented = 0;
+                }
+            } else if (c == '*') {
+                asterisk = 1;
             }
-          } else if (c == '/') {
-            slashed = 1;
-            continue;
-          }
-          if (c == '\'') {
-            singlequoted = 1;
-          }
-          if (c == '\"') {
+        } else if (doublequoted) {
+            if (escaped) {
+                escaped = 0;
+            } else {
+                if (c == '\"') {
+                    doublequoted = 0;
+                }
+                if (c == '\\') {
+                    escaped = 1;
+                }        
+            } 
+            putchar(c);
+        } else if (c == '\"') {
             doublequoted = 1;
-          }
+            putchar(c);
+        } else if (singlequoted) {
+            if (c == '\'') {
+                singlequoted = 0;
+            }
+            putchar(c);
+        } else if (c == '\'') {
+            singlequoted = 1;
+            putchar(c);
+        } else if (slashed) {
+            if (c == '*') {
+                slashed = 0;
+                commented = 1;
+            } else {
+                putchar('/');
+                putchar(c);
+                slashed = 0;
+            }
+        } else if (c == '/') {
+            slashed = 1;
+        } else {
+            putchar(c);
         }
-      }
-      putchar(c);
-    } 
-   
-  }
 
-  return 0;
+    }
+
+    return 0;
 }
