6bcc40c5f6e5d4ac6f2fd5ea427a0f0035d9ecb3
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-09-16 21:13:31 -0300

    simpler version that doesn't care about spliting words in half

--- chapter-1/1-22-fold.c
+++ chapter-1/1-22-fold.c
@@ -1,64 +1,41 @@
 #include <stdio.h>
-#define OUT 0
-#define IN 1
+
+#define MAXLINE 1000
 #define TEXTWIDTH 80
-#define MAXWORDLENGTH 1000
 
-
-int wi, c, numchars;
-char lastword[MAXWORDLENGTH];
-
-void printfolded();
+int mygetline(char s[], int lim);
 
 int main() {
-  int i, state;
-  
-  for (i=0; i<MAXWORDLENGTH; i++) {
-    lastword[i] = ' ';
+  int nparts, i, j, len;
+  char line[MAXLINE];
+
+  while ((len = mygetline(line, MAXLINE)) > 0) {
+    if (len <= TEXTWIDTH) {
+      printf("%s", line);
+      continue;
+    }
+    nparts = (len / TEXTWIDTH) + 1;
+    for (i = 0; i < nparts; i++) {
+      for (j = i*TEXTWIDTH; j < TEXTWIDTH*(i+1); j++) {
+        putchar(line[j]);
+      }
+      putchar('\n');
+    }
+
   }
 
-  state = OUT;
-  numchars = 0;
-  wi = 0; 
-  while ((c = getchar()) != EOF) {
-    ++numchars;
-    if (c == ' ') {
-      if (state == IN) {  // just finished recording last word?
-        printfolded();
-        for (i=0; i<=wi; i++) { // erase lastword array
-          lastword[i] = ' ';
-        }
-        wi = 0;
-      } else {
-        if (numchars > TEXTWIDTH) {
-          putchar('\n');
-        }
-        putchar(c);
-      }
-      state = OUT;
-    } else if (state == OUT) {
-      state = IN;
-    }
-    
-    if (c != ' ') {
-      lastword[wi] = (char) c;
-      ++wi;
-    }
-  }
-  printfolded();
+  return 0;
 }
 
-void printfolded() {
-  lastword[wi+1] = '\0';
-  if (numchars > TEXTWIDTH) {  // line lenght so far > TEXTWIDTH ?
-    printf("\n%s ", lastword);  // break the line before printing the word
-    numchars = 0;
-  } else if (numchars == (TEXTWIDTH + 1)) {  // if last word completed TEXTWIDTH chars in a line, print the word without a trailing space
-    printf("%s\n", lastword);
-    numchars = 0;
-  } else if (c == EOF){
-    printf("%s", lastword);
-  } else {  // just a word in the middle of the line? just print it! with the current space added to the end
-    printf("%s ", lastword);
+int mygetline(char s[], int lim) {
+  int c, i;
+
+  for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+    s[i] = (char) c;
+  if (c == '\n') {
+    s[i] = (char) c;
+    ++i;
   }
+  s[i] = '\0';
+  return i;
 }
