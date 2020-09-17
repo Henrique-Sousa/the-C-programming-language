b742912cfc7961181800ce8d4bba8485335add50
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-09-17 15:55:00 -0300

    handle quoted strings

--- chapter-1/1-23-comments.c
+++ chapter-1/1-23-comments.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
 
 int main() {
-  int c, inquote, inslash, incomment, inasterisk;
+  int c, inquote, indoublequote, inbackslash, inslash, incomment, inasterisk;
   
-  inquote = inslash = incomment = inasterisk = 0;
+  inbackslash = indoublequote = inquote = inslash = incomment = inasterisk = 0;
 
   while ((c = getchar()) != EOF) {
     if (incomment == 0) {
-      if (inquote == 0) {
-        if (inslash == 1) {
-          if (c == '*') {
-            inslash = 0;
-            incomment = 1;
+      if (indoublequote == 0) {
+        if (inquote == 0) {
+          if (inslash == 1) {
+            if (c == '*') {
+              inslash = 0;
+              incomment = 1;
+              continue;
+            }
+          } else if (c == '/') {
+            inslash = 1;
             continue;
           }
-        } else if (c == '/') {
-          inslash = 1;
-          continue;
+          if (c == '\'') {
+            inquote = 1;
+          }
+          if (c == '\"') {
+            indoublequote = 1;
+          }
+        } else if (c == '\'') {
+          inquote = 0;
         }
-        if (c == '\'') {
-          inquote = 1;
+      } else if (inbackslash == 0) {
+        if (c == '\"') {
+          indoublequote = 0;
         }
-      } else if (c == '\'') {
-        inquote = 0;
+        if (c == '\\') {
+          inbackslash = 1;
+        }    
+      } else {
+        inbackslash = 0;
       }
       putchar(c);
     } else {
