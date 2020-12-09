e219b96c67b4444f7f7413f4756f66ba40a1a3bd
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-12-09 19:40:31 -0300

    feature: add the function in the other direction

--- chapter-3/3-02-escape.c
+++ chapter-3/3-02-escape.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 void escape(char* s, char* t);
+void unescape(char* s, char* t);
 
 int main() {
     char* s = "first sentence\nsecond sentence\n\tindented sentence";
     char t[100];
+    char r[100];
     printf("unescaped:\n%s\n", s);
     escape(s, t);
     printf("\nescaped:\n%s\n", t);
+    unescape(t, r);
+    printf("\nescaped again:\n%s\n", r);
     return 0;
 }
 
@@ -29,6 +33,29 @@ void escape(char* s, char* t) {
                 t[j] = s[i];
                 j++;
                 break;
+        }
+    }
+    t[j] = '\0';
+}
+
+void unescape(char* s, char* t) {
+    int i, j;
+    for (i = j = 0; s[i] != '\0'; i++) {
+        if (s[i] == '\\') {
+            i ++;
+            switch(s[i]) {
+                case 'n':
+                    t[j] = '\n';
+                    j++;
+                    break;
+                case 't':
+                    t[j] = '\t';
+                    j++;
+                    break;
+            }
+        } else {
+            t[j] = s[i];
+            j++;
         }
     }
     t[j] = '\0';
