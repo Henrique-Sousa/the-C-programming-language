06901d38dbd99e7d33397bd87f55dc09244776e0
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-12-09 18:59:08 -0300

    3-2. escape
    Exercise 3-2. Write a function escape(s, t) that converts characters
    like newline and tab into visible escape sequences like \n and \t as it
    copies the string t to s. Use a switch. Write a function for the other
    direction as well, converting escape sequences into the real characters.

---
+++ chapter-3/3-02-escape.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void escape(char* s, char* t);
+
+int main() {
+    char* s = "first sentence\nsecond sentence\n\tindented sentence";
+    char t[100];
+    printf("unescaped:\n%s\n", s);
+    escape(s, t);
+    printf("\nescaped:\n%s\n", t);
+    return 0;
+}
+
+void escape(char* s, char* t) {
+    int i, j;
+    for (i = j = 0; s[i] != '\0'; i++) {
+        switch(s[i]) {
+            case '\n':
+                t[j] = '\\';
+                t[j+1] = 'n';
+                j += 2;
+                break;
+            case '\t':
+                t[j] = '\\';
+                t[j+1] = 't';
+                j += 2;
+                break;
+            default:
+                t[j] = s[i];
+                j++;
+                break;
+        }
+    }
+    t[j+1] = '\0';
+}
