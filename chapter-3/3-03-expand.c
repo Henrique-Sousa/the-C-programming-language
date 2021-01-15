f55e548a6765d8abaf6a164bd5303bd8e81bf486
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-01-14 22:11:31 -0300

    feature: add ability to handle cases like a-b-c and -a-z

--- chapter-3/3-03-expand.c
+++ chapter-3/3-03-expand.c
@@ -3,10 +3,18 @@
 void expand(char* s1, char* s2);
 
 int main() {
-    char* range = "a-z0-9";
-    char result[100];
-    expand(range, result);
-    printf("%s\n", result);
+    char* range1 = "a-zA-Z0-9";
+    char* range2 = "a-f-z";
+    char* range3 = "-a-jbn-q-";
+    char result1[100];
+    char result2[100];
+    char result3[100];
+    expand(range1, result1);
+    expand(range2, result2);
+    expand(range3, result3);
+    printf("%s\n", result1);
+    printf("%s\n", result2);
+    printf("%s\n", result3);
     
     return 0;
 }
@@ -18,11 +26,19 @@ void expand(char* s1, char* s2) {
 
     j = 0;
     for (i = 0; s1[i] != '\0'; i++) {
-        if (s1[i+1] == '-') {
+        if (s1[i] == '-') {
+            s2[j] = '-';
+            j++;
+        }
+        else if (s1[i+1] == '-' && s1[i+2] != '\0') {
             for (letter = s1[i], end = s1[i+2]; letter <= end; j++, letter++) {
                 s2[j] = letter; 
             }
             i+=2;
+        }
+        else {
+            s2[j] = s1[i];
+            j++;
         }
     }
 
